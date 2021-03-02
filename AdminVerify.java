657de8e6621ad02f4b8e3adfadc016b50a9dd0cb
soham6125 <70870489+soham6125@users.noreply.github.com>
2021-03-02 18:37:52 +0530

    Add files via upload

---
+++ AdminVerify.java
@@ -0,0 +1,14 @@
+public class AdminVerify 
+{
+	
+	private int AdminId = 111;
+	private String AdminPassword="admin";
+	
+	public int getUserId() {
+		return AdminId;
+	}
+	public String getPassword() {
+		return AdminPassword;
+	}
+	
+}
